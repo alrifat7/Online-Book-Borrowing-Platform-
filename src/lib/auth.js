33c9995bec91346718937b94f9dd1c9b6d8e2c64
Rifat <alrifat421@gmail.com>
2026-05-04 23:17:15 +0600

    fix mongodb connection

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,19 +4,21 @@
 
     const client = new MongoClient(process.env.MONGODB_URI);
 
+    await client.connect(); // 🔥 important
+
     const db = client.db("OnlineBookBorrowingPlatform");
 
     export const auth = betterAuth({
-    database: mongodbAdapter(db, { 
-        client 
+    database: mongodbAdapter(db, {
+        client,
     }),
     emailAndPassword: {
         enabled: true,
     },
     socialProviders: {
         google: {
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }
-    }
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        },
+    },
     });
